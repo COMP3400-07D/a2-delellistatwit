1ec44a49c5ecf706d93d5c0baad4b441ab40f20e
Tyler deLellis <delellist@wit.edu>
2025-10-09 19:01:45 -0400

    Completed array_create

--- array_create.c
+++ array_create.c
@@ -1,9 +1,40 @@
 #include <stddef.h> // For NULL
-// TODO: Include any necessary header files here
+#include <stdlib.h> // For malloc, free
+#include <stdbool.h> // For true/false
 
 /**
- * TODO: Describe what the function does
+ * Creates an array of even integers from begin to end (inclusive).
+ * If begin is odd, starts from the next even number.
+ * Returns NULL if begin > end or if memory allocation fails.
  */
 int* array_create_evens(int begin, int end) {
-    return NULL;
+    if (begin > end) 
+    {
+        return NULL;
+    }
+
+    if (begin % 2 != 0) 
+    {
+        begin++;
+    }
+
+    if (begin > end) 
+    {
+        return NULL;
+    }
+
+    int count = ((end - begin) / 2) + 1;
+
+    int* arr = malloc(sizeof(int) * count);
+    if (arr == NULL) 
+    {
+        return NULL;
+    }
+
+    for (int i = 0; i < count; i++) 
+    {
+        arr[i] = begin + (i * 2);
+    }
+
+    return arr;
 }
