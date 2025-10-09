40f09fac19c3abc94cf27e6e2ff1f5130f28c6bd
Tyler deLellis <delellist@wit.edu>
2025-10-09 19:05:10 -0400

    Completed string fill.

--- string_fill.c
+++ string_fill.c
@@ -1,7 +1,21 @@
 #include <stddef.h> // For NULL
 
 /**
- * TODO: Describe what the function does
+ * Fills the destination array with character 'c' and null-terminates it.
+ * Only fills up to dest_cap - 1 characters to leave space for the '\0' terminator.
+ * If dest is NULL or dest_cap <= 0, does nothing.
  */
-void string_fill(char dest[], int dest_cap, char c) {
+    void string_fill(char dest[], int dest_cap, char c) 
+{
+    if (dest == NULL || dest_cap <= 0) {
+        return;
+    }
+
+    int i;
+    for (i = 0; i < dest_cap - 1; i++) {
+        dest[i] = c;
+    }
+
+    // Null-terminate the string
+    dest[i] = '\0';
 }
